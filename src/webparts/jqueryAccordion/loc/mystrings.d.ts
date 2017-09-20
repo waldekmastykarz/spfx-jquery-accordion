1b3ae1a24629e5079f99531337309640cbe290d3
Chakkaradeep (Chaks) CC <chakkaradeep@users.noreply.github.com>
2017-09-19 22:14:41 -0700

    jQuery accordion web part complete

---
+++ src/webparts/jqueryAccordion/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IJqueryAccordionWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'JqueryAccordionWebPartStrings' {
+  const strings: IJqueryAccordionWebPartStrings;
+  export = strings;
+}
